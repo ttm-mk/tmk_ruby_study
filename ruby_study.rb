181d7fabbb6fe62fe3d5fb6f4f00dfc32f8f2454
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-10-03 01:37:55 +0000

    [Update]ruby_study

--- ruby_study.rb
+++ ruby_study.rb
@@ -1,1 +1,37 @@
-puts "こんにちは"
+# # UFO演算子
+
+# puts 100 <=> 10
+
+# puts 100 <=> 100
+
+# p 10 <=> 100
+
+# # 乗
+
+# a = 100
+
+# puts a **= 1
+
+# # クラス
+# # R3は１はINTEGERクラスになってる/確認
+
+# p 1.class
+# p 10000000000000000.class
+# p 1.2.class
+# p 1.class.superclass
+
+# メソッド
+
+def test(a=0, b=100)
+  a + b
+end
+
+p test
+
+def bar(a:, b: 100, **z)
+    p z
+    # ↑これがあるからハッシュが表示される
+    a + b
+end
+
+p bar(a: 2,b: 200, c: 100, d: 200)
