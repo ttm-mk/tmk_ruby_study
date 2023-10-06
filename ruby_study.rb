e4670c7ab85770acc29c3824d8e194307f7722c3
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-10-06 13:57:51 +0000

    Update

--- ruby_study.rb
+++ ruby_study.rb
@@ -19,19 +19,46 @@
 # p 10000000000000000.class
 # p 1.2.class
 # p 1.class.superclass
+# a = []
+# p a.class.superclass
+# p 42/10r
+# p (42/10r).class
+# p 42i.class
 
 # メソッド
 
-def test(a=0, b=100)
-  a + b
+# def test(a=0, b=100)
+#   a + b
+# end
+
+# p test
+
+# def bar(a:, b: 100, **z)
+#     p z
+#     # ↑これがあるからハッシュが表示される
+#     a + b
+# end
+
+# p bar(a: 2,b: 200, c: 100, d: 200)
+
+# # 文法
+# $foo = 1
+# def bar
+#   $foo
+# end
+
+# p bar
+# p $baz
+
+# p 0x10
+
+a = if true
+  1
 end
 
-p test
+p a
 
-def bar(a:, b: 100, **z)
-    p z
-    # ↑これがあるからハッシュが表示される
-    a + b
-end
-
-p bar(a: 2,b: 200, c: 100, d: 200)
+a = 1 if true
+p a
+a = 2 if false
+p 2
